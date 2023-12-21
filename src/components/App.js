04b1e7b1f8d52782bdbfc3ffd2f47d155b803591
Tony Eder <tonyeder11@gmail.com>
2023-12-21 15:54:59 -0800

    plants rendering on front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  //set default state of plants listed on page with blank array
+  const [ plants, setPlants ] = useState([])
+
+  useEffect (() => {
+    fetch('http://localhost:6001/plants')
+    .then((res) => res.json())
+    .then((plants) => setPlants(plants))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} />
     </div>
   );
 }
