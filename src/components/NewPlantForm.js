d6cd9daff0ce4da9593e4c7ba117b7c1ec05e7b4
Tony Eder <tonyeder11@gmail.com>
2023-12-21 16:58:11 -0800

    add new plant form activated

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ plants, onAddPlant }) {
+  const [ name, setName ] = useState("")
+  const [ image, setImage ] = useState("")
+  const [ price, setPrice ] = useState(0)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const plantData = {
+      key: plants.id,
+      name: name,
+      image: image,
+      price: price,
+    }
+    fetch ('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify(plantData)
+    })
+    .then((r) => r.json())
+    .then((newPlant) => onAddPlant(newPlant))
+    
+  }
+
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setName(e.target.value)} value={name} type="text" name="name" placeholder="Plant name" />
+        <input onChange={(e) => setImage(e.target.value)} value={image} type="text" name="image" placeholder="Image URL" />
+        <input onChange={(e) => setPrice(e.target.value)} value={price} type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -3,10 +3,15 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage({ plants }) {
+function PlantPage({ plants, setPlants }) {
+
+  function onAddPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm plants={plants} onAddPlant={onAddPlant} />
       <Search />
       <PlantList plants={plants} />
     </main>
