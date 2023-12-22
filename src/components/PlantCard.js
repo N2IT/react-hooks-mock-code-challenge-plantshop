76fdaad19a968627cb4d209e6ce234a3ae62ec68
Tony Eder <tonyeder11@gmail.com>
2023-12-21 17:50:05 -0800

    stock button functioning

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
-  const {id, image, name, price} = plant
+  const { id, image, name, price, inStock } = plant;
+  const [isInStock, setIsInStock] = useState(true);
+
+  function handleStockClick() {
+    setIsInStock(!isInStock); // Toggle the state when clicked
+    console.log('clicked');
+  }
 
   return (
     <li key={id} className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button onClick={handleStockClick} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockClick}>Out of Stock</button>
       )}
     </li>
   );
